bf2b5127f9ce5b94dddc538abfe3143de317ebf1
GRIFFIN Boudreau <79546684+InfectedGriffon@users.noreply.github.com>
2022-03-01 16:49:19 +0000

    why

--- change/script.js
+++ change/script.js
@@ -2,7 +2,7 @@
 window.onload = function() {
 	var a = document.getElementById("styler");
   a.onclick = function() {
-	document.getElementById(center).style.color = "#b00b69";
+document.getElementById(center).style.color = '#b00b69';
 		return false;
 	}
 }
