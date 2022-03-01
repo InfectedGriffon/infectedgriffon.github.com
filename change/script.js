623898bfe9eaed38f28f7d59f89d13bc7fb3d322
GRIFFIN Boudreau <79546684+InfectedGriffon@users.noreply.github.com>
2022-03-01 16:38:23 +0000

    what does dom mean

---
+++ change/script.js
@@ -0,0 +1,8 @@
+ 
+window.onload = function() {
+	var a = document.getElementById("styler");
+  a.onclick = function() {
+	document.getElementById(center).style.color = "#b00b69";
+		return false;
+	}
+}
