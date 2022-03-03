2389b5a00275949c2bbdb980d0e1fe0110b30863
InfectedGriffon <79546684+InfectedGriffon@users.noreply.github.com>
2022-03-02 19:11:54 -0500

    Update script.js
    more images

--- rps/script.js
+++ rps/script.js
@@ -5,25 +5,30 @@ function rps() {
   switch (yourroll) {
     case 1:
       document.getElementById("youthrew").innerHTML = "rock";
-       document.getElementById("yourimage").src = "images/rock-red.png";
+      document.getElementById("yourimage").src = "images/rock-red.png";
       break;
     case 2:
       document.getElementById("youthrew").innerHTML = "paper";
+      document.getElementById("yourimage").src = "images/paper-red.png";
       break;
     case 3:
       document.getElementById("youthrew").innerHTML = "scissors";
+      document.getElementById("yourimage").src = "scissors/rock-red.png";
       break;
   }
 
   switch (theirroll) {
     case 1:
       document.getElementById("theythrew").innerHTML = "rock";
+      document.getElementById("yourimage").src = "images/rock-blue.png";
       break;
     case 2:
       document.getElementById("theythrew").innerHTML = "paper";
+      document.getElementById("yourimage").src = "images/paper-blue.png";
       break;
     case 3:
       document.getElementById("theythrew").innerHTML = "scissors";
+      document.getElementById("yourimage").src = "scissors/rock-blue.png";
       break;
   }
 }
