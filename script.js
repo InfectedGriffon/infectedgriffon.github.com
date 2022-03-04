aacf31b4cce7e7768f401af910add261c88c7182
GRIFFIN Boudreau <79546684+InfectedGriffon@users.noreply.github.com>
2022-03-04 15:53:53 +0000

    ominouser

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 var countDownDate = new Date("April 1, 2022 00:00:00").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function() {
 
-  // Get today's date and time
   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("timeleft").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("demo").innerHTML = days + " Days<br> " + hours + " Hours<br> "
+  + minutes + " Minutes<br> " + seconds + " Seconds<br> ";
 
-  // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("timeleft").innerHTML = "EXPIRED";
