b8decc7285b5448545b811d86da7b637ee11a9a0
GRIFFIN Boudreau <79546684+InfectedGriffon@users.noreply.github.com>
2022-03-21 14:40:45 +0000

    comments

--- rps/script.js
+++ rps/script.js
@@ -1,9 +1,8 @@
 function rps() {
-	const hands = ['rock', 'paper', 'scissors'];
-  var yourroll = Math.floor(Math.random() * 3);
+	const hands = ['rock', 'paper', 'scissors']; //list of hand symbols
+  var yourroll = Math.floor(Math.random() * 3); //randomize each hand
   var theirroll = Math.floor(Math.random() * 3);
 
- 
   document.getElementById("youthrew").innerHTML = hands[yourroll];
   document.getElementById("yourimage").src = "images/" + hands[yourroll] + "-red.png";
 
