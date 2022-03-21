ec7c246b0610d8fd30f3d0225a80cb0ac06cb8ea
GRIFFIN Boudreau <79546684+InfectedGriffon@users.noreply.github.com>
2022-03-21 14:20:02 +0000

    why switch when can array

--- rps/script.js
+++ rps/script.js
@@ -1,34 +1,12 @@
 function rps() {
+	const hands = ['nothing', 'rock', 'paper', 'scissors'];
   var yourroll = Math.floor(Math.random() * 3);
   var theirroll = Math.floor(Math.random() * 3);
 
-  switch (yourroll) {
-    case 1:
-      document.getElementById("youthrew").innerHTML = "rock";
-      document.getElementById("yourimage").src = "images/rock-red.png";
-      break;
-    case 2:
-      document.getElementById("youthrew").innerHTML = "paper";
-      document.getElementById("yourimage").src = "images/paper-red.png";
-      break;
-    case 3:
-      document.getElementById("youthrew").innerHTML = "scissors";
-      document.getElementById("yourimage").src = "scissors/rock-red.png";
-      break;
-  }
+ 
+  document.getElementById("youthrew").innerHTML = hands[yourroll];
+  document.getElementById("yourimage").src = "images/" + hands[yourroll] + "-red.png";
 
-  switch (theirroll) {
-    case 1:
-      document.getElementById("theythrew").innerHTML = "rock";
-      document.getElementById("theirimage").src = "images/rock-blue.png";
-      break;
-    case 2:
-      document.getElementById("theythrew").innerHTML = "paper";
-      document.getElementById("theirimage").src = "images/paper-blue.png";
-      break;
-    case 3:
-      document.getElementById("theythrew").innerHTML = "scissors";
-      document.getElementById("theirimage").src = "scissors/rock-blue.png";
-      break;
-  }
+	document.getElementById("theithrew").innerHTML = hands[theirtoll];
+  document.getElementById("theirimage").src = "images/" + hands[theirroll] + "-red.png";  
 }
