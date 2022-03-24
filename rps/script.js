18377ec9e03e2fe3887307e27f030db02823783e
GRIFFIN Boudreau <79546684+InfectedGriffon@users.noreply.github.com>
2022-03-24 21:40:50 +0000

    line breaks

--- rps/script.js
+++ rps/script.js
@@ -14,16 +14,16 @@ function rps() {
 
 	//find winner
 	if (yourroll == theirroll) {
-		document.getElementById("winner").innerHTML = "Winner: Tie"; //tie
+		document.getElementById("winner").innerHTML = "Winner:<br>Tie"; //tie
 	} else if(yourroll == theirroll+2 || yourroll == theirroll-1) {
 		/*
 			1 - 3 > rock beats scissors
 			2 - 1 > paper beats rock
 			3 - 2 > scissors beats paper
 		*/
-		document.getElementById("winner").innerHTML = "Winner: Left"; //left side wins
+		document.getElementById("winner").innerHTML = "Winner:<br>Left"; //left side wins
 	} else {
-		document.getElementById("winner").innerHTML = "Winner: Right"; //right side wins
+		document.getElementById("winner").innerHTML = "Winner:<br>Right"; //right side wins
 	}
 	
 }
