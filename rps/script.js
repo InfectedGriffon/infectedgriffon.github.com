f9db819703c320cf71b5dca7fc37225dbdcb9415
GRIFFIN Boudreau <79546684+InfectedGriffon@users.noreply.github.com>
2022-03-24 21:03:03 +0000

    rps even more thingy

--- rps/script.js
+++ rps/script.js
@@ -2,6 +2,7 @@ function rps() {
 	const hands = ['rock', 'paper', 'scissors']; //list of hand symbols
   var yourroll = Math.floor(Math.random() * hands.length); //pick a random symbol for left side
   var theirroll = Math.floor(Math.random() * hands.length); //pick a random symbol for right side
+	
 
 	//change text and image to the symbol rolled
   document.getElementById("youthrew").innerHTML = hands[yourroll];
@@ -10,4 +11,16 @@ function rps() {
 	//same for right side
 	document.getElementById("theythrew").innerHTML = hands[theirroll];
   document.getElementById("theirimage").src = "images/" + hands[theirroll] + "-blue.png";  
+
+	//find winner
+	if (yourroll = theirroll) {
+		//tie
+	} else {
+		var winner = Boolean(yourroll == theirroll+2 || yourroll == theirroll-1); //true for left, false for right
+		//if its two more or one less than right they win
+		//1-3 > rock beats scissors
+		//2-1 > paper beats rock
+		//3-2 > scissors beats paper
+	}
+	
 }
