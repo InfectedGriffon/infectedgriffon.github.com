335a49ef304fa6a4a299d21ec00cf2d93eeaf8f7
GRIFFIN Boudreau <79546684+InfectedGriffon@users.noreply.github.com>
2022-03-24 20:42:16 +0000

    formatting fixes

--- rps/script.js
+++ rps/script.js
@@ -1,13 +1,13 @@
 function rps() {
 	const hands = ['rock', 'paper', 'scissors']; //list of hand symbols
-  var yourroll = Math.floor(Math.random() * 3);
-  var theirroll = Math.floor(Math.random() * 3);
+  var yourroll = Math.floor(Math.random() * hands.length); //pick a random symbol for left side
+  var theirroll = Math.floor(Math.random() * hands.length); //pick a random symbol for right side
 
+	//change text and image to the symbol rolled
   document.getElementById("youthrew").innerHTML = hands[yourroll];
-  document.getElementById("yourimage").src = 
-		"images/" + hands[yourroll] + "-red.png";
+  document.getElementById("yourimage").src = "images/" + hands[yourroll] + "-red.png";
 
+	//same for right side
 	document.getElementById("theythrew").innerHTML = hands[theirroll];
-  document.getElementById("theirimage").src = 
-		"images/" + hands[theirroll] + "-blue.png";  
+  document.getElementById("theirimage").src = "images/" + hands[theirroll] + "-blue.png";  
 }
